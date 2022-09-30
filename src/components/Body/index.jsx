e1e25523ea61a94ccdf4f025bc390892479a4e5b
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2022-09-30 19:47:32 +0200

    Add a camera frame and styled

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -4,10 +4,20 @@ import "./body.css"
 
 function Body({params}) {
 
+    let h1html = null
+    let lefthtml = null
+
+    if (params.title !== null) {
+        h1html =  <h1 className="body-title">{params.title}</h1>
+    } else {
+        lefthtml = <div className="cam-frame"></div>
+    }
+
     if (params) {
         return (
             <section className="home-body">
-                <h1 className="body-title">{params.title}</h1>
+                {h1html}
+                {lefthtml}
             </section>
         )
     }
