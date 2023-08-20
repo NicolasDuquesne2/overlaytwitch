641e8e13c70bef1351b7fc62d59001be6151ad00
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2023-08-20 13:24:32 +0200

    Set concrete texts

--- src/params/Texts.jsx
+++ src/params/Texts.jsx
@@ -1,20 +1,20 @@
 export const texts = {
     home: {
         title: "Le stream va bientôt débuter",
-        music: "music title",
-        composer: "music composer"
+        music: "Last Slate Of The Roof",
+        composer: "Doctor Turtle"
     },
     game: {
         title: "jeu en cours"
     },
     ending: {
         title: "Fin de live ! Merci de votre passage",
-        music: "music title",
-        composer: "music composer"
+        music: "Salt Lake Swerve",
+        composer: "Maarten Schellekens"
     },
     break: {
         title: "Juste le temps d'une pause. A tout de suite",
-        music: "music title",
-        composer: "music composer" 
+        music: "Last Slate Of The Roof",
+        composer: "Doctor Turtle"
     }
 }
