a53075e1f4d7c82c5af8758af6239bec3e730d0b
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2023-08-20 13:58:40 +0200

    fix missing params files

---
+++ src/params/Texts.jsx
@@ -0,0 +1,20 @@
+export const texts = {
+    home: {
+        title: "Le stream va bientôt débuter",
+        music: "Last Slate Of The Roof",
+        composer: "Doctor Turtle"
+    },
+    game: {
+        title: "jeu en cours"
+    },
+    ending: {
+        title: "Fin de live ! Merci de votre passage",
+        music: "Salt Lake Swerve",
+        composer: "Maarten Schellekens"
+    },
+    break: {
+        title: "Juste le temps d'une pause. A tout de suite",
+        music: "Last Slate Of The Roof",
+        composer: "Doctor Turtle"
+    }
+}
